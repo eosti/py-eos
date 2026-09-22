a4bf1c16a0f2b400db8e300f8bdde7db3b68e8c7
Reid Sox-Harris <reid@soxharris.com>
2026-09-22 01:39:20 -0400

    ref: cleanup pyproject

--- tests/test_cues.py
+++ tests/test_cues.py
@@ -1,11 +1,12 @@
-from decimal import Decimal
 import random
 import time
+from decimal import Decimal
 
 import pytest
 
 from eos import Cue, Eos
 from eos.helpers import EosError
+
 
 @pytest.fixture
 def test_cue(eos: Eos):
@@ -15,18 +16,10 @@ def test_cue(eos: Eos):
     eos.cues.delete(q)
 
 
-
 @pytest.mark.parametrize(
-        ("cue_list", "cue_num"),
-        [
-            (1, 1),
-            (1, Decimal("1.7")),
-            (2, 2),
-            (2, Decimal("2.7"))
-            ]
-        )
+    ("cue_list", "cue_num"), [(1, 1), (1, Decimal("1.7")), (2, 2), (2, Decimal("2.7"))]
+)
 class TestEosCuesCreationDeletion:
-
     @pytest.fixture(scope="class", autouse=True)
     def remove_cuelists(self, eos: Eos):
         yield
@@ -84,15 +77,15 @@ class TestEosCuesCreationDeletion:
         end_num_cues = eos.cues.count(cue_list)
         assert end_num_cues == start_num_cues - 1
 
+
 class TestEosCueModification:
     def test_cue_record_existing(self, eos: Eos, test_cue):
         with pytest.raises(EosError):
             eos.cues.record(test_cue)
 
     @pytest.mark.parametrize(
-            ("cue_label"),
-            [("pytest"), ("pytest new label"), ("label #3"), ("label # ! ?")]
-            )
+        ("cue_label"), [("pytest"), ("pytest new label"), ("label #3"), ("label # ! ?")]
+    )
     def test_cue_label(self, eos: Eos, test_cue, cue_label):
         before_props = eos.cues.get_cue(test_cue)
         assert before_props.label == ""
@@ -116,10 +109,10 @@ class TestEosCueModification:
         time.sleep(0.05)
         after_props = eos.cues.get_cue(test_cue)
         assert after_props.uptime == 8
-        assert after_props.downtime == Decimal('7.9')
+        assert after_props.downtime == Decimal("7.9")
+
 
 class TestEosCueExternalConnections:
-
     @pytest.fixture
     def target_cue(self, eos: Eos):
         q = Cue(1, random.randint(1000, 1999))
@@ -136,7 +129,7 @@ class TestEosCueExternalConnections:
         eos.cues.link_cue(test_cue, test_cue)
         time.sleep(1)
 
-        after_props = eos.cues.get_cue(test_cue)
+        eos.cues.get_cue(test_cue)
         # Seemingly Eos doesn't do this anymore?
 
     def test_cue_external_actions_cue(self, eos: Eos, test_cue):
@@ -152,14 +145,13 @@ class TestEosCueExternalConnections:
         assert after_props.actions[0] == f"Q{test_cue.cuelist} / {test_cue.cue}"
 
 
-
 class TestEosCueCurrentState:
     cue_list = (
-            Cue(1, 1, label="Cue One", duration=Decimal(6)),
-            Cue(1, 2, label="Cue Two", duration=Decimal("4.9")),
-            Cue(1, 9, label="Cue Tres", duration=Decimal(3)),
-            Cue(1, 999, label="Cue Last", duration=Decimal(2))
-            )
+        Cue(1, 1, label="Cue One", duration=Decimal(6)),
+        Cue(1, 2, label="Cue Two", duration=Decimal("4.9")),
+        Cue(1, 9, label="Cue Tres", duration=Decimal(3)),
+        Cue(1, 999, label="Cue Last", duration=Decimal(2)),
+    )
 
     @pytest.fixture(scope="class", autouse=True)
     def setup_cuelist(self, eos: Eos):
@@ -173,7 +165,6 @@ class TestEosCueCurrentState:
         yield
         eos.keys.live()
         eos.send_command("Delete Cue 1 / # #")
-
 
     def test_current_cue(self, eos: Eos) -> None:
         assert eos.cues.previous_cue is None
